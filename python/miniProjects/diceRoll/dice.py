fad723dbd5548f76d1c70286a862d82702911d58
Meitsuki <dzhu.inc@gmail.com>
2018-07-27 07:35:56 +0000

    Updated diceRolling project

--- python/miniProjects/diceRoll/dice.py
+++ python/miniProjects/diceRoll/dice.py
@@ -5,18 +5,23 @@ class dice:
         self.numSides = numSides
 
     def rollDice(self):
-        return random.randint(1,numSides)
+        return random.randint(1,self.numSides)
 
 
 class diceContainer:
-    def __init__(self, numDice, sameNumSides = True, numSideForDice):
+    def __init__(self, numDice, sameNumSides, numSideForDice):
         self.numDice = numDice
         self.sameNumSides = sameNumSides
         self.numSideForDice = numSideForDice
 
-        if sameNumSides:
-            for x in range(numDice):
-                diceContainer.append(dice(numSideForDice[0]))
+        if self.sameNumSides:
+            for x in range(self.numDice):
+                self.diceContainer.append(dice(numSideForDice[0]))
         else:
-            for x in range(numDice):
-                diceContainer.append(dice(numSideForDice[x]))
+            for x in range(self.numDice):
+                self.diceContainer.append(dice(numSideForDice[x]))
+
+    def rollDiceContainer(self):
+        for x in range(self.numDice):
+            self.diceContainerRollResults.append(self.diceContainer[x].rollDice())
+        return self.diceContainerRollResults
--- python/miniProjects/diceRoll/diceRollSimulation.py
+++ python/miniProjects/diceRoll/diceRollSimulation.py
@@ -3,9 +3,26 @@ import dice
 print("Welcome to the dice roll simulator program!")
 validPrompt = False
 
-numDice = input("To get started, how many dice would you like to roll? ")
+while not validPrompt:
+    numDice = input("To get started, how many dice would you like to roll? ")
+    validInt = False
+    try:
+        numDice = int(numDice) + 0
+        validInt = True
+    except TypeError:
+        validInt = False
+    if validInt:
+        if numDice >= 1:
+            validPrompt = True
+        else:
+            print("Please provide a positive integer.")
+    else:
+        print("You have provided an invalid input, please enter a positive integer.")
+
 print("Excellent, you wish to roll " + str(numDice) + " dice.")
+
 validPrompt = False
+sameNumSides = False
 while not validPrompt:
     sameNumSides = input("Would you like these dice to all have the same number of sides? ")
     if sameNumSides.lower() == "yes" or sameNumSides.lower() == "no":
@@ -13,4 +30,18 @@ while not validPrompt:
     else:
         print("You have provided an invalid answer. Please enter Yes or No.")
 
+if sameNumSides.lower() == "yes":
+    sameNumSides = True
+else:
+    sameNumSides = False
+
+validIntPrompt = False
+if sameNumSides:
+    numSides = input("How many sides do you wish the dice to have? )
+else:
+    for x in range(numDice):
+        numSides = input("How many sides do you wish dice " + str(x) + " to have? ")
+        sidesForDice.append(numSides)
+
+dices = diceContainer(numDice, sameNumSides, sidesForDice)
 print("Have a nice day.")
